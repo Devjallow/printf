657bb5523ce2705d3d948d705f05edb56fe3c128
Mustapha <tplmustyabu@gmail.com>
2022-07-14 11:56:26 +0100

    putchar

--- _putchar.c
+++ _putchar.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - writes the char c to stdout
+ * @c: The char to print to stdout
  *
  * Return: On success 1.
  * On error, -1 is return, and errno is set appropriately.
